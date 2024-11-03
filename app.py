792e2be5ef7e61dcdf50e5d0440aab526843d71b
allmamun556 <35899262+allmamun556@users.noreply.github.com>
2024-11-03 16:50:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import RandomForestRegressor
 
 # Load the trained Random Forest model (replace 'random_forest_model.pkl' with the actual model file path)
 model = pickle.load(open('random_forest_model.pkl', 'rb'))
-print("mamaun")
+
 
 
 # Streamlit App
