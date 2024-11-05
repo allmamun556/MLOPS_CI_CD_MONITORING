b107a94b2095fe3634e695b225da5b5c4f6ed4da
allmamun556 <allmamun556@gmail.com>
2024-11-05 18:22:36 +0100

    added

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,6 @@
 import unittest
 import pandas as pd
+import os
 
 """ Only on etest is written here. In future more test case ccould be written here"""
 
@@ -8,7 +9,7 @@ class TestDataFunctions(unittest.TestCase):
 
     def test_load_data(self):
         # Test that load_data successfully loads a CSV and returns a DataFrame
-        test_file_path = '../data/Turbine_Data.csv'
+        test_file_path = os.path.join(os.path.dirname(__file__), '../data/Turbine_Data.csv')
 
         # Load the data using the function
         loaded_data = pd.read_csv(test_file_path)
