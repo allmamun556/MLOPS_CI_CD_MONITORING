d119bbe3ea73d9f367afef2fa47350a88200bab6
allmamun556 <allmamun556@gmail.com>
2024-11-26 19:43:55 +0100

    added

---
+++ src/test_model.py
@@ -0,0 +1,20 @@
+import unittest
+import pandas as pd
+import os
+
+""" Only on etest is written here. In future more test case ccould be written here"""
+
+
+class TestDataFunctions(unittest.TestCase):
+
+    def test_load_data(self):
+        # Test that load_data successfully loads a CSV and returns a DataFrame
+       # test_file_path = os.path.join(os.path.dirname(__file__), '../data/Turbine_Data.csv')
+
+        # Load the data using the function
+        loaded_data = pd.read_csv('data/Turbine_Data.csv')
+        self.assertEqual(loaded_data.shape[1], 22, "DataFrame should have 22 columns")
+
+   
+if __name__ == "__main__":
+    unittest.main()
